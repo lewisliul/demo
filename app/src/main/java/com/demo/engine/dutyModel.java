86ee9e98fc151dfa0c0a0eb8c71b0829633fc497
liuhao <1729818779@qq.com>
2016-07-29 18:52:32 +0800

    [增加engine业务逻辑包，view自定义包]

---
+++ app/src/main/java/com/demo/engine/dutyModel.java
@@ -0,0 +1,7 @@
+package com.demo.engine;
+
+/**
+ * Created by hc on 2016/7/29.
+ */
+public class dutyModel {
+}
---
+++ app/src/main/java/com/demo/view/MyView.java
@@ -0,0 +1,7 @@
+package com.demo.view;
+
+/**
+ * Created by hc on 2016/7/29.
+ */
+public class MyView {
+}
